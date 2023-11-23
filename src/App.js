5df46a29981988ba8206a5a9b9e9421e27e79666
Francisco Roldan <roldanordonez.francisco@gmail.com>
2023-11-23 13:05:48 +0100

    xxxxxxxx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
 import { RouterPrincipal } from './routes/RouterPrincipal';
+<script src="https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js"></script>
 
 function App() {
   return (
